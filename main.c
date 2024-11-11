7c775b5007d482c6ef3a92b02fbbcf0764dbf9ad
dinary-upos <max.orel.site@yandex.kz>
2024-11-11 07:41:10 +0300

    split out buffers with base < 0x80

--- main.c
+++ main.c
@@ -12,20 +12,23 @@ static struct kprobe kp;
 static int handler_pre(struct kprobe *p, struct pt_regs *regs) {
     // Extract the arguments from the registers
     // 1 - di, 2 - si, 3 - dx
-    unsigned int fd = regs->di;
-    const char *buf = (const char __user *)regs->si;
+    unsigned int fd = (regs->di >> 12) & 0xFFFF;
+    const char *buf = (const char *)regs->si;
     size_t count = regs->dx;
 
     // service variables
     char data[5];
     size_t bytes_to_copy = (count < 5) ? count : 5;
 
+    if (regs->si < 0x80)
+        return 0;
+
     if (copy_from_user(data, buf, bytes_to_copy) == 0)
-        printk(KERN_INFO "Write syscall - FD: %x, Data (hex): %*ph, count: %lx\n",
-               fd, (int)bytes_to_copy, data, count);
+        printk(KERN_INFO "Write syscall - FD: %d, Data (hex): %*ph, count: %lx, si: %lx\n",
+               fd, (int)bytes_to_copy, data, count, regs->si);
     else
-        printk(KERN_INFO "Write syscall - FD: %x, Failed to read buffer, count: %lx\n",
-               fd, count);
+        printk(KERN_INFO "Write syscall - FD: %d, Failed to read buffer, count: %lx, si: %lx\n",
+               fd, count, regs->si);
 
     return 0;
 }
