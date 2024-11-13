6f8607042727c9d4a14f9a9ad3b450b061be81c1
dinary-upos <max.orel.site@yandex.kz>
2024-11-13 03:50:32 +0300

    almost ready
    writing to hand-made proc ring buffer enabled

--- header.h
+++ header.h
@@ -1,4 +1,25 @@
 #ifndef VARS_H
 #define VARS_H
 
+#include <linux/module.h>
+#include <linux/uaccess.h>
+#include <linux/proc_fs.h>
+
+#define COPY_BUF_SIZE 40
+#define ENTRY_SIZE 512
+#define BUFFER_SIZE 262144
+
+struct ring_buffer {
+    char *data;
+    size_t head, tail, size;
+};
+extern struct ring_buffer rbuf;
+void ring_buffer_init(struct ring_buffer *buffer);
+void ring_buffer_append(struct ring_buffer *buffer, const char *values, size_t length);
+
+ssize_t proc_read(struct file *file, char __user *buffer, size_t count, loff_t *pos);
+static const struct proc_ops proc_fops = {
+    .proc_read = proc_read,
+};
+
 #endif // VARS_H
--- main.c
+++ main.c
@@ -1,16 +1,15 @@
-#include <linux/module.h>
 #include <linux/kprobes.h>
-#include <linux/uaccess.h>
 #include <linux/printk.h>
 #include <linux/file.h>
+#include "header.h"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("max.orel.site@yandex.kz");
 MODULE_DESCRIPTION("Kprobe example to track 'write' syscall");
 
-#define COPY_BUF_SIZE 40
-
 static struct kprobe kp;
+struct ring_buffer rbuf;
+static struct proc_dir_entry *proc_entry;
 
 static int copy_middle(char *to, const char *from, size_t count) {
     if (count == 0)
@@ -32,23 +31,21 @@ static int handler_pre(struct kprobe *p, struct pt_regs *regs) {
     // taken from declaration of 'vfs_write' function
     struct file *file = (struct file *)regs->di;
     const char *buf = (const char *)regs->si;
-    size_t count = regs->dx;
-    loff_t *pos = (loff_t *)regs->cx;
+    size_t count = (size_t)regs->dx;
+    loff_t *ppos = (loff_t *)regs->cx;
 
     // buf to copy from userspace
-    char kbuf[COPY_BUF_SIZE];
-    char filename[PATH_MAX];
-    char *path;
+    char kbuf[COPY_BUF_SIZE], filename[PATH_MAX], *path, entry[ENTRY_SIZE];
     int write_count;
-    loff_t file_size;
+    loff_t file_size, pos = *ppos;
 
     // we want work only with writes on real files
     if (!(file && S_ISREG(file->f_inode->i_mode)))
         return 0;
 
-    // check if we have append (pos == file_size)
+    // skip if we have append
     file_size = file->f_inode->i_size;
-    if (file_size != 0 && *pos == file_size)
+    if (file_size != 0 && pos == file_size)
         return 0;
 
     // extract filename from 'struct file'
@@ -60,8 +57,10 @@ static int handler_pre(struct kprobe *p, struct pt_regs *regs) {
         strstr(path, "/run"))
         return 0;
 
-    if ((write_count = copy_middle(kbuf, buf, count)))
-        printk(KERN_INFO "Written file: %s, data: %*ph\n", path, write_count, kbuf);
+    if ((write_count = copy_middle(kbuf, buf, count))) {
+        sprintf(entry, "Written file: %s, data: %*ph\n", path, write_count, kbuf);
+        ring_buffer_append(&rbuf, entry, strlen(entry));
+    }
 
     return 0;
 }
@@ -70,20 +69,32 @@ static int handler_pre(struct kprobe *p, struct pt_regs *regs) {
 static int __init my_kprobe_init(void) {
     int ret;
 
+    rbuf.data = kmalloc(BUFFER_SIZE, GFP_KERNEL);
+    if (!rbuf.data)
+        return -ENOMEM;
+    ring_buffer_init(&rbuf);
+
+    proc_entry = proc_create("my_proc", 0444, NULL, &proc_fops);
+
     kp.symbol_name = "vfs_write";
     kp.pre_handler = handler_pre;
 
     ret = register_kprobe(&kp);
     if (ret < 0) {
         printk(KERN_INFO "Failed to register kprobe: %d\n", ret);
+        proc_remove(proc_entry);
+        kfree(rbuf.data);
         return ret;
     }
+
     printk(KERN_INFO "Kprobe registered for write syscall\n");
     return 0;
 }
 
 static void __exit my_kprobe_exit(void) {
     unregister_kprobe(&kp);
+    proc_remove(proc_entry);
+    kfree(rbuf.data);
     printk(KERN_INFO "Kprobe unregistered\n");
 }
 
